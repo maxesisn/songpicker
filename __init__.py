9805312de6403f189e8c72631f399ec05c27bf90
Maximilian Wu <me@maxesisn.online>
2020-08-31 23:29:49 +0800

    大体结构完成

--- __init__.py
+++ __init__.py
@@ -8,6 +8,9 @@ sv = Service("点歌")
 async def pick_song(bot, ev):
     song_name = ev.message.extract_plain_text()
     if not song_name:
-        await bot.send(ev,"要加上歌曲名才能点歌！")
+        await bot.send(ev, "要加上歌曲名才能点歌！")
         return
-    search_result=requests.get('')
+    search_result = requests.get(
+        f'http://127.0.0.1:3000/search?keywords={song_name}')
+    song_id = search_result['result']['songs'][0]['id']
+    await bot.send(ev, f"[CQ:music,type=163,id={song_id}]")
